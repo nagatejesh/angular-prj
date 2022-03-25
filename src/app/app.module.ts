b8ae60d0c40a72ba3fb87d9c3a4f22ce8c45d5b8
nagatejesh <nagatejesh2@gmail.com>
2022-03-25 15:46:12 +0530

    added login and signup paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,24 @@ import { Routes, RouterModule } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AnimeComponent } from './anime/anime.component';
+import { HelpComponent } from './help/help.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const approutes:Routes=[
   {path:'',component:HomeComponent},
-  {path:'anime', component:AnimeComponent}
+  {path:'anime', component:AnimeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'help', component:HelpComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent}
 ]
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
     RouterModule.forRoot(approutes) ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, HomeComponent,AnimeComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, HomeComponent,AnimeComponent, HelpComponent, AboutComponent, LoginComponent, SignupComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/help/help.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector:'app-help',
+  templateUrl:'./help.component.html'
+})
+
+export class HelpComponent{
+  
+}
